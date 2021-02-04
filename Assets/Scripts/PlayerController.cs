0b9bb2e675c3e9a11b3eb773dee2814d987682ef
tehckisnow <chetwonsik@gmail.com>
2021-02-04 18:25:38 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,189 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.U2D;
+
+public class PlayerController : MonoBehaviour
+{
+    public float walkSpeed;
+    public float runSpeed;
+    public Collider2D interactionPoint;
+
+    private Rigidbody2D rb;
+    private Animator animator;
+    private Facing facing = Facing.Down;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        SetInteractionPoint();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckInput();
+    }
+
+    private void CheckInput()
+    {
+        //get input
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        bool running = Input.GetAxis("Fire3") > 0;
+        
+        //set animation
+        if(horizontalInput != 0 || verticalInput != 0)
+        {
+            SetInteractionPoint();
+            DetermineFacing(horizontalInput, verticalInput);
+            if(running)
+            {animator.speed = 2;}
+            SetWalkingAnim();
+        }
+        else
+        {
+            animator.speed = 1;
+            SetIdleAnim();
+        }
+
+        //movement
+        float speed = walkSpeed;
+        if(running)
+        {
+            speed = runSpeed;
+        }
+        Vector2 newVelocity = new Vector2(horizontalInput * speed, verticalInput * speed);
+        
+        rb.velocity = newVelocity;
+    }
+
+    private void DetermineFacing(float horizontal, float vertical)
+    {
+        if(Mathf.Abs(horizontal) > Mathf.Abs(vertical))
+        { //horizontal
+            if(horizontal > 0)
+            { //right
+                facing = Facing.Right;
+            }
+            else //left
+            {
+                facing = Facing.Left;
+            }
+        }
+        else //vertical
+        {
+            if(vertical < 0)
+            { //down
+                facing = Facing.Down;
+            }
+            else
+            { //up
+                facing = Facing.Up;
+            }
+        }
+    }
+
+    private void SetIdleAnim()
+    {
+        switch(facing)
+        {
+            case Facing.Down:
+                animator.Play("IdleDown");
+                break;
+            case Facing.Up:
+                animator.Play("IdleUp");
+                break;
+            case Facing.Left:
+                animator.Play("IdleLeft");
+                break;
+            case Facing.Right:
+                animator.Play("IdleRight");
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void SetWalkingAnim()
+    {
+        switch(facing)
+        {
+            case Facing.Down:
+                animator.Play("WalkDown");
+                break;
+            case Facing.Up:
+                animator.Play("WalkUp");
+                break;
+            case Facing.Left:
+                animator.Play("WalkLeft");
+                break;
+            case Facing.Right:
+                animator.Play("WalkRight");
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void SetInteractionPoint()
+    {
+        switch(facing)
+        {
+            case Facing.Down:
+                interactionPoint.transform.localPosition = new Vector3(0, -1, 0);
+                break;
+            case Facing.Up:
+                interactionPoint.transform.localPosition = new Vector3(0, 1, 0);
+                break;
+            case Facing.Left:
+                interactionPoint.transform.localPosition = new Vector3(-1, 0, 0);
+                break;
+            case Facing.Right:
+                interactionPoint.transform.localPosition = new Vector3(1, 0, 0);
+                break;
+            default:
+                break;
+        }
+    }
+
+    //check for step triggers
+    // private void TriggerCheck()
+    // {
+    //     List<Collider2D> results = new List<Collider2D>();
+    //     ContactFilter2D filter = new ContactFilter2D();
+    //     filter.useTriggers = true;
+    //     if(thisCollider.OverlapCollider(filter, results) > 0)
+    //     {
+    //         Trigger target = results[0].gameObject.GetComponent<Trigger>();
+    //         if(target != null)
+    //         {
+    //             target.Activate();
+    //         }
+    //     }
+    // }
+
+    private void InteractionCheck()
+    {
+        List<Collider2D> results = new List<Collider2D>();
+            if(interactionPoint.OverlapCollider(new ContactFilter2D(), results) > 0)
+            {
+                Interaction target = results[0].gameObject.GetComponent<Interaction>();
+                if(target != null)
+                {
+                    target.Activate();
+                };
+            }
+    }
+}
+
+public enum Facing
+{
+    Up,
+    Down,
+    Left,
+    Right
+}
---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,38 @@
+﻿//! consider making an interface
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    bool active = false;
+
+    public virtual void Activate()
+    {
+        if(!active)
+        {
+            active = true;
+            Debug.Log(this.name + " triggered.");
+        }
+    }
+
+    public virtual void Callback()
+    {
+
+    }
+
+    // void OnTriggerEnter2D(Collider2D other)
+    // {
+    //     PlayerController controller = other.GetComponent<PlayerController>();
+
+    //     if (controller != null)
+    //     {
+    //         //if(controller.currentHealth < controller.maxHealth)
+    //         //{
+    //         //controller.ChangeHealth(1);
+    //         Destroy(gameObject);
+    //         //}
+    //     }
+    // }
+}
