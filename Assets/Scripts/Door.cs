79220f5a538f094c593f039c6a001da86a9a962a
tehckisnow <chetwonsik@gmail.com>
2021-06-14 20:31:39 -0400

    Extracted persistence utility into new component

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -16,35 +16,46 @@ public class Door : Trigger
     [SerializeField] private Sprite closedSprite;
 
     private string id = "door";
+    public Persistence persistence;
 
     void Start()
     {
-        GenerateId();
-        if(GameManager.instance.persistentData.Exists(id, "isOpen"))
-        {
-            if(GameManager.instance.persistentData.GetData(id, "isOpen"))
-            {
-                OpenDoor();
-            }
-            else
-            {
-                CloseDoor();
-            }
-        }
-        else
-        {
-            GameManager.instance.persistentData.RegisterValue(id, "isOpen", isOpen);
-        }
+        //GenerateId();
+        //SetInitialState();
     }
 
-    private void GenerateId()
-    {
-        //id += SceneManager.GetActiveScene().name += transform.position.x.ToString() += transform.position.y.ToString();
-        string scenename = SceneManager.GetActiveScene().name;
-        string x = transform.position.x.ToString();
-        string y = transform.position.y.ToString();
-        id += scenename += x += y;
-    }
+    // private void SetInitialState()
+    // {
+    //     if(GameManager.instance.persistentData.Exists(id, "isOpen"))
+    //     {
+    //         if(GameManager.instance.persistentData.GetData(id, "isOpen"))
+    //         {
+    //             OpenDoor();
+    //         }
+    //         else
+    //         {
+    //             CloseDoor();
+    //         }
+    //     }
+    //     else
+    //     {
+    //         GameManager.instance.persistentData.RegisterValue(id, "isOpen", isOpen);
+    //     }
+    // }
+
+    // private void GenerateId()
+    // {
+    //     //id += SceneManager.GetActiveScene().name += transform.position.x.ToString() += transform.position.y.ToString();
+    //     string scenename = SceneManager.GetActiveScene().name;
+    //     string x = transform.position.x.ToString();
+    //     string y = transform.position.y.ToString();
+    //     id += scenename += x += y;
+    // }
+
+    // private void SetState(string key, bool state)
+    // {
+    //     GameManager.instance.persistentData.RegisterValue(id, key, state);
+    // }
 
     public override void Activate()
     {
@@ -57,23 +68,41 @@ public class Door : Trigger
             PlayerController player = GameManager.instance.player.GetComponent<PlayerController>();
             if(player.obtainables[ObtainableTypes.Keys] > 0)
             {
-                //GameManager.instance.persistentData.SetData(id, "isOpen", true);
-                GameManager.instance.persistentData.RegisterValue(id, "isOpen", true);
+                
+                //GameManager.instance.persistentData.RegisterValue(id, "isOpen", true);
+                persistence.SetState("main", true);
+                //gameObject.GetComponent<Persistence>().SetState("main", true);
+
                 player.obtainables[ObtainableTypes.Keys] -= 1;
-                OpenDoor();
-                isOpen = true;
+                //OpenDoor();
+                TrueState();
             }
         }
     }
 
-    private void OpenDoor()
+    // private void OpenDoor()
+    // {
+    //     GetComponent<SpriteRenderer>().sprite = openedSprite;
+    //     GetComponent<Collider2D>().isTrigger = true;
+    //     isOpen = true;
+    // }
+
+    // private void CloseDoor()
+    // {
+    //     GetComponent<SpriteRenderer>().sprite = closedSprite;
+    //     GetComponent<Collider2D>().isTrigger = false;
+    //     isOpen = false;
+    // }
+
+//!----------------------------------
+    public void TrueState()
     {
         GetComponent<SpriteRenderer>().sprite = openedSprite;
         GetComponent<Collider2D>().isTrigger = true;
         isOpen = true;
     }
 
-    private void CloseDoor()
+    public void FalseState()
     {
         GetComponent<SpriteRenderer>().sprite = closedSprite;
         GetComponent<Collider2D>().isTrigger = false;
---
+++ Assets/Scripts/Persistence.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Persistence : MonoBehaviour
+{
+    //[SerializeField] private Component component;
+    [SerializeField] private MonoBehaviour component;
+    [SerializeField] private bool startingState = false;
+
+    private string id;
+    private PersistentData persistentData;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        persistentData = GameManager.instance.persistentData;
+
+        //!component.persistence = this;
+        component.gameObject.GetComponent<Door>().persistence = this;
+
+        GenerateId();
+        SetInitialState();
+    }
+
+    public void SetState(string key, bool state)
+    {
+        persistentData.RegisterValue(id, key, state);
+    }
+
+    private void GenerateId()
+    {
+        string scene = SceneManager.GetActiveScene().name;
+        string name = gameObject.name;
+        string x = transform.position.x.ToString();
+        string y = transform.position.y.ToString();
+        id += scene += name += x += y;
+    }
+
+    private void SetInitialState()
+    {
+        if(persistentData.Exists(id, "main"))
+        {
+            if(persistentData.GetData(id, "main"))
+            {
+                //!component.TrueState();
+                //component.SendMessage("TrueState");
+                component.Invoke("TrueState", 0f);
+                //component.gameObject.GetComponent<Door>().TrueState();
+            }
+            else
+            {
+                //!component.FalseState();
+                //component.SendMessage("FalseState");
+                component.Invoke("FalseState", 0f);
+                //component.gameObject.GetComponent<Door>().FalseState();
+            }
+        }
+        else
+        {
+            persistentData.RegisterValue(id, "main", startingState);
+        }
+    }
+
+}
